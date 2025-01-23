13e9db7fbd9332262f15fe5c5bff2b9e999ec995
cyloic <l.cyusa@alustudent.com>
2025-01-23 05:30:33 -0800

    ML

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -1,19 +1,38 @@
 #!/usr/bin/env python3
-""" defines function that performs matrix multiplication """
+""" defines function that concatenates two 2D matrices along an axis """
 
 
-def mat_mul(mat1, mat2):
-    """ returns new matrix that is the product of two 2D matrices """
-    mat1_columns = len(mat1[0])
-    mat2_rows = len(mat2)
-    if mat1_columns != mat2_rows:
-        return None
-    new_matrix = []
-    for row_count, row in enumerate(mat1):
-        new_matrix.append([])
-        for column_count in range(len(mat2[0])):
-            dot = 0
-            for index in range(mat1_columns):
-                dot += (mat1[row_count][index] * mat2[index][column_count])
-            new_matrix[row_count].append(dot)
-    return new_matrix
+def cat_matrices2D(mat1, mat2, axis=0):
+    """ returns new matrix that is the concatenation of two 2D matrices """
+    if axis is 0:
+        # concatenate rows
+        for row in mat1:
+            mat1_columns = len(row)
+        for row in mat2:
+            mat2_columns = len(row)
+        if mat1_columns != mat2_columns:
+            return None
+        cat_matrix = []
+        for index1, row in enumerate(mat1):
+            cat_matrix.append([])
+            for i in row:
+                cat_matrix[index1].append(i)
+        index1 += 1
+        for index2, row in enumerate(mat2):
+            cat_matrix.append([])
+            for i in row:
+                cat_matrix[index1 + index2].append(i)
+        return cat_matrix
+    if axis is 1:
+        # concatenates columns
+        if len(mat1) != len(mat2):
+            return None
+        cat_matrix = []
+        for index, row in enumerate(mat1):
+            cat_matrix.append([])
+            for i in mat1[index]:
+                cat_matrix[index].append(i)
+            for i in mat2[index]:
+                cat_matrix[index].append(i)
+        return cat_matrix
+    return None
